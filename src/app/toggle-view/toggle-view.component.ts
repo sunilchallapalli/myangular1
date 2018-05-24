24ae79f9a7d282baf04816fafff05c299ee9df57
Sunil Challapalli <sunil84.sap@gmail.com>
2018-05-24 21:43:40 +0530

    chnaged Content

--- src/app/toggle-view/toggle-view.component.ts
+++ src/app/toggle-view/toggle-view.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ToggleViewComponent implements OnInit {
 
   showContent: boolean = true;
-  showContent1: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
